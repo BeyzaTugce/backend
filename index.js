65cf2d91da2dcf200bc7349fcf7638b55cbc833c
Bikem Çamli <ge37zeb@mytum.de>
2021-06-04 19:42:00 +0200

    index.js modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-const CONNECTION_URL = "mongodb+srv://team_03:<Team03**>@team03.tqhf8.mongodb.net/Team03?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://team_03:<Team03**>@team03.tqhf8.mongodb.net/MyGarage?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
