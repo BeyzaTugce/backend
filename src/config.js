c22029f5380dddbc7cdef1e6f7d7e64ef73b819c
Beyza Bilgic <beyza.bilgic@tum.de>
2022-01-24 01:31:11 +0300

    mongoURI changed

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 // Configuration variables
 const port = process.env.PORT || "4000";
-const mongoURI = process.env.MONGODB_URI || "mongodb+srv://team_03:hejYg2CEeKJ08XH9@team03.tqhf8.mongodb.net/MyGarage?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGODB_URI || "mongodb+srv://team_03:<password>@team03.tqhf8.mongodb.net/MyGarage?retryWrites=true&w=majority";
 const JwtSecret = process.env.JWT_SECRET || "very secret secret";
 
 
